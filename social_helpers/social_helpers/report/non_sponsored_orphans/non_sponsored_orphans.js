a96d7a5736eb0b8eec21b443fe0b5981622386ca
xXConnorXx-095 <oussama.ouaydah@gmail.com>
2023-06-24 15:50:02 +0300

    sponsored and non sponsored orphans report

---
+++ social_helpers/social_helpers/report/non_sponsored_orphans/non_sponsored_orphans.js
@@ -0,0 +1,21 @@
+frappe.query_reports["Test 234"] = {
+  filters: [
+    {
+      fieldname: "gender",
+      label: __("Gender"),
+      fieldtype: "Select",
+      options: [
+        { value: "Male", label: __("Male") },
+        { value: "Female", label: __("Female") },
+      ],
+      default: "Male",
+    },
+    {
+      fieldname: "date_on",
+      label: __("On"),
+      fieldtype: "Date",
+      options: "",
+      default: "Today",
+    },
+  ],
+};
