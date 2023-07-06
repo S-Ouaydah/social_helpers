f62f8b22c87efa9aa3510a0f6c21888fcf87edbb
xXConnorXx-095 <oussama.ouaydah@gmail.com>
2023-07-07 02:10:57 +0300

    Family

--- social_helpers/social_helpers/doctype/family/family.js
+++ social_helpers/social_helpers/doctype/family/family.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2023, muslims IT and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Family', {
-	// refresh: function(frm) {
+// frappe.ui.form.on("Family", {
+// 	refresh(frm) {
 
-	// }
-});
+// 	},
+// });
